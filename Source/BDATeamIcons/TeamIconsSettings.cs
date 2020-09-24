4f96d8536fc8931f7c964377987cf91bffe43704
SuicidalInsanity <origamimage@gmail.com>
2020-09-24 13:13:14 -0700

    Adds new settings

--- Source/BDATeamIcons/TeamIconsSettings.cs
+++ Source/BDATeamIcons/TeamIconsSettings.cs
@@ -1,4 +1,4 @@
-﻿namespace BDTeamIcons
+namespace BDTeamIcons
 {
 	public class TeamIconSettings
 	{
@@ -7,27 +7,12 @@
 		[SettingsDataField] public static bool TEAMICONS = true;
 		[SettingsDataField] public static bool TEAMNAMES = false;
 		[SettingsDataField] public static bool VESSELNAMES = true;
+		[SettingsDataField] public static bool SCORE = false;
+		[SettingsDataField] public static bool HEALTHBAR = false;
 		[SettingsDataField] public static bool MISSILES = false;
 		[SettingsDataField] public static bool DEBRIS = true;
 		[SettingsDataField] public static bool PERSISTANT = true;
 		[SettingsDataField] public static bool POINTERS = true;
 		[SettingsDataField] public static float ICONSCALE = 1.0f;
-
-		[SettingsDataField] public static string TEAM_1_COLOR = "255,0,0,200";
-		[SettingsDataField] public static string TEAM_2_COLOR = "0,0,255,200";
-		[SettingsDataField] public static string TEAM_3_COLOR = "0,255,0,200";
-		[SettingsDataField] public static string TEAM_4_COLOR = "255,255,0,200";
-		[SettingsDataField] public static string TEAM_5_COLOR = "255,192,0,200";
-		[SettingsDataField] public static string TEAM_6_COLOR = "255,0,255,200";
-		[SettingsDataField] public static string TEAM_7_COLOR = "0,255,255,200";
-		[SettingsDataField] public static string TEAM_8_COLOR = "0,128,0,200";
-		[SettingsDataField] public static string TEAM_9_COLOR = "255,192,192,200";
-		[SettingsDataField] public static string TEAM_10_COLOR = "128,0,128,200";
-		[SettingsDataField] public static string TEAM_11_COLOR = "0,128,128,200";
-		[SettingsDataField] public static string TEAM_12_COLOR = "128,128,255,200";
-		[SettingsDataField] public static string TEAM_13_COLOR = "128,96,0,200";
-		[SettingsDataField] public static string TEAM_14_COLOR = "192,192,128,200";
-		[SettingsDataField] public static string TEAM_15_COLOR = "255,128,0,200";
-		[SettingsDataField] public static string TEAM_16_COLOR = "0,0,128,200";
 	}
 }
